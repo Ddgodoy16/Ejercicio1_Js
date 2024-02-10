0c00425ae14b78b7c10754fe92df27f5e8276179
Ddgodoy16 <elvinnu_es25@outlook.es>
2024-02-09 22:52:46 -0600

    Revert "codigo completo"
    This reverts commit 8c5e86c866f7e77f34965c8a2ebb13bc5b0bc6cf.

--- js/Index.js
+++ js/Index.js
@@ -1,38 +1,18 @@
 function calcular() {
-  let nota1 = parseFloat(document.getElementById("nota1").value);
-  let nota2 = parseFloat(document.getElementById("nota2").value);
-  let nota3 = parseFloat(document.getElementById("nota3").value);
-  
+  let num1 = parseFloat(document.getElementById("num1").value);
+  let num2 = parseFloat(document.getElementById("num2").value);
+  var resultado;
 
+  if (isNaN(num1) || isNaN(num2)) {
+    error("Por favor, ingrese un número válido.")
+    
+    return;
 
- 
-  if (isNaN(nota1) || isNaN(nota2) || isNaN(nota3)) {
-       error("Por favor, ingrese un número válido para todas las notas.");
-  } else if (nota1 > 30 || nota2 > 30 || nota3 > 40) {
-       error("Las primeras dos  notas no deben ser mayor a 30%  y 40% para la tercera nota.");
   } else {
-        let suma = nota1 + nota2 + nota3;
-        let mensaje = "";
-        if (suma >= 0 && suma <= 59) {
-          mensaje = "Reprobado";
-        } else if (suma >= 60 && suma <= 79) {
-          mensaje = "Bueno";
-        } else if (suma >= 80 && suma <= 89) {
-          mensaje = "Muy Bueno";
-        } else if (suma >= 90 && suma <= 100) {
-          mensaje = "Sobresaliente";
-        } else {
-               error("La suma de las notas está fuera de rango.");
-          return; 
-        }
-        
-       
-        document.getElementById("textP").textContent = "Su nota Obtenida es:  " + suma;
-        document.getElementById("textP1").textContent = "Observación:  " + mensaje;
-  
+    resultado = num1 * num2;
   }
-
-}
+  document.getElementById("textP").textContent = "El Area del Rectangulo Es: " + resultado;
+};
 
 function error(text) {
   Swal.fire({
@@ -44,8 +24,7 @@ function error(text) {
 
 
 function limpiarFormulario() {
-  document.getElementById("nota1").value = " ";
-  document.getElementById("nota2").value = " ";
-  document.getElementById("nota3").value = " ";
+  document.getElementById("num1").value = " ";
+  document.getElementById("num2").value = " ";
 
 }
